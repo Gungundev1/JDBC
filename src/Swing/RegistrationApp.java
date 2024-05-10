d7c9d913e9f9cdef18e9c62a8402e2ce08190e39
Gungun agrawal <agrawalgungun859@gmail.com>
2024-05-10 16:22:11 +0530

    Registration app

---
+++ src/Swing/RegistrationApp.java
@@ -0,0 +1,79 @@
+package Swing;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+public class RegistrationApp {
+    public static void main(String[] args) {
+        JFrame f = new JFrame();
+
+        JFrame jFrame=new JFrame("Registration app");
+
+        JLabel heading=new JLabel("Print data on console....");
+        heading.setBounds(70,20,180,40);
+
+        JLabel firstName=new JLabel("First Name");
+        firstName.setBounds(20,50,80,20);
+
+        JLabel lastName = new JLabel("Last name");
+        lastName.setBounds(20,75,80,30);
+
+        JLabel dob=new JLabel("Date-of-Birth");
+        dob.setBounds(20,105,80,30);
+
+        JTextField firstNameTf=new JTextField();
+        firstNameTf.setBounds(120,50,100,20);
+
+        JTextField lastNameTf=new JTextField();
+        lastNameTf.setBounds(120, 80, 100, 20);
+
+        JTextField  dobtf=new JTextField();
+        dobtf.setBounds(120, 110, 100, 20);
+
+        JButton sbmt = new JButton("Submit");
+        sbmt.setBounds(20, 160, 100, 30);
+
+        sbmt.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String name = firstNameTf.getText();
+                String lName = lastNameTf.getText();
+                String dob = dobtf.getText();
+                System.out.println(name);
+                System.out.println(lName);
+                System.out.println(dob);
+            }
+        });
+
+        JButton reset = new JButton("Reset");
+        reset.setBounds(140,160,100,30);
+        reset.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                firstNameTf.setText("");
+                lastNameTf.setText("");
+                dobtf.setText("");
+            }
+        });
+
+        f.add(heading);
+        f.add(firstName);
+        f.add(lastName);
+        f.add(dob);
+        f.add(firstNameTf);
+        f.add(lastNameTf);
+        f.add(dobtf);
+        f.add(sbmt);
+        f.add(reset);
+
+        f.setSize(500,400);
+        f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        f.setLayout(null);
+        f.setVisible(true);
+    }
+
+    }
+
---
+++ src/Swing/SwingAppbox.java
@@ -0,0 +1,14 @@
+package Swing;
+
+import javax.swing.*;
+
+
+
+class Swing{
+    public static void main(String[] args) {
+        JFrame jFrame=new JFrame("Swing app");
+        jFrame.setSize(600,600);
+        jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        jFrame.setVisible(true);
+    }
+}
